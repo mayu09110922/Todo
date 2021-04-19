0df3f5ff8e6a6628d623e59141b8b1c94914d998
mayu09110922 <akisaku03@gmail.com>
2021-04-19 09:09:26 +0900

    [add]トップ画面

---
+++ database/migrations/2021_04_17_170940_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 50);
+            $table->text('body');
+            $table->timestamps();
+            $table->timestamp('deleted_at');
+            $table->date('limit');
+            $table->timestamp('copy');
+            $table->timestamp('move');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
---
+++ resources/views/Task/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Todo</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <h1>ToDo</h1>
+        <div class='todos'>
+            <div class='todo'>
+            <h2 class='title'>やること</h2>
+            <h2 class='title'>持ち物リスト</h2>
+            <h2 class='title'>お金</h2>
+            </div>
+        </div>
+    </body>
+    </html>
